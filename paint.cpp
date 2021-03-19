309c6adba5a8af0fc6af39445cb88cc55ab5a2ca
Alex <52524449+kmem1@users.noreply.github.com>
2021-03-19 15:03:30 +0300

    Update paint.cpp

--- paint.cpp
+++ paint.cpp
@@ -1,9 +1,7 @@
 /******************************************************************************
-
                               Online C++ Compiler.
                Code, Compile, Run and Debug C++ program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
 *******************************************************************************/
 
 #include <iostream>
@@ -85,6 +83,6 @@ int main()
     }
     
     paint(1, 1);
-    cout << n_walls;
+    cout << n_walls * 9;
     return 0;
 }
