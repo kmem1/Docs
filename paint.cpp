c842c065cc4fb006e7bb8cdce0dac6e18737632f
Alex <52524449+kmem1@users.noreply.github.com>
2021-03-19 12:59:19 +0300

    Update paint.cpp

--- paint.cpp
+++ paint.cpp
@@ -14,6 +14,7 @@ const char empty_char = '.';
 const int empty = 0;
 const char rock_char = '#';
 const int rock = 1;
+const int checked = 2;
 
 const int max_n = 35;
 
@@ -22,31 +23,26 @@ int field[max_n][max_n];
 int n_walls = 0;
 
 void paint(int i, int j) {
-    cout << i << " " << j << endl;
-    if (n_walls == 22)
-        return;
-    if (i < 1 || i >= max_n)
-        return;
-    if (j < 1 || j >= max_n)
-        return;
-    if (field[i][j] != empty) {
-        cout << "ahhahahahahahahahahahhahaha" << endl;
-        return;
+    if (field[i][j] == empty) {
+        field[i][j] = checked;
+        if (i < 1 || i >= max_n)
+            return;
+        if (j < 1 || j >= max_n)
+            return;
+        if (field[i+1][j] == rock)
+            n_walls++;
+        if (field[i-1][j] == rock)
+            n_walls++;
+        if (field[i][j+1] == rock)
+            n_walls++;
+        if (field[i][j-1] == rock)
+            n_walls++;
+            
+        paint(i-1, j);
+        paint(i+1, j);
+        paint(i, j+1);
+        paint(i, j-1);
     }
-    if (field[i+1][j] == rock)
-        n_walls++;
-    if (field[i-1][j] == rock)
-        n_walls++;
-    if (field[i][j+1] == rock)
-        n_walls++;
-    if (field[i][j-1] == rock)
-        n_walls++;
-        
-    paint(i-1, j);
-    paint(i+1, j);
-    paint(i, j+1);
-    paint(i, j-1);
-
 }
 
 int main()
